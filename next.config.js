20bcf756250ca59e2f7d62d7ed7504e24e7ba144
Taiyu Yoshizawa <taiyu31@gmail.com>
2021-12-31 00:57:30 +0900

    init commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '~': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
+};
